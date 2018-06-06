7ddeb78b6093d165eba904d1355b819529dc28af
nenadV91 <nenadwebdevelop@gmail.com>
2018-06-06 17:51:47 +0200

    Small style updates.

--- src/views/dashboard/widgets/GeoChart.jsx
+++ src/views/dashboard/widgets/GeoChart.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { Chart } from 'react-google-charts';
 import { colors } from 'utils';
+import { Spin, Icon } from 'antd';
+
 
 
 class GeoChart extends Component {
   render() {
+    let antIcon = <Icon 
+    spin 
+    type="loading" 
+    style={{ fontSize: 24 }} />
+
     const chartOptions = {
       legend: false,
       backgroundColor: "",
@@ -14,8 +21,10 @@ class GeoChart extends Component {
         colors.chartMedium
       ]}
     }
+    
     return (
       <Chart
+      loader={<Spin indicator={antIcon} />}
       chartType="GeoChart"
       options={chartOptions}
       data={this.props.data}
